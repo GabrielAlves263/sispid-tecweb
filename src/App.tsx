f568400a2476e989671de2124c7c03de6e3695c6
Gabriel Alves <jgabrielalves@alu.ufc.br>
2026-06-15 18:50:16 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500">
+        React + TS + Tailwind
+      </h1>
+    </div>
+  )
+}

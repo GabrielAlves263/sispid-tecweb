7d5addc091d795510d2ca6130eb89679641de258
Gabriel Alves <jgabrielalves@alu.ufc.br>
2026-06-15 19:16:55 -0300

    feat: add login page and input field component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import LoginPage from "./pages/LoginPage";
+
 export default function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-blue-500">
-        React + TS + Tailwind
-      </h1>
-    </div>
+    <>
+      <LoginPage></LoginPage>
+    </>
   )
 }
